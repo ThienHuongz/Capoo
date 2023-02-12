2270b77d18162a425a4fb785898b60fa9ce2adcb
EgaoZz <96277683+EgaoZz@users.noreply.github.com>
2023-02-12 20:42:45 +0700

    Add files via upload

---
+++ project/game.java
@@ -0,0 +1,14 @@
+package project;
+
+import javax.swing.*;
+
+public class game {
+    public static void main(String[] args) {        
+         
+        JFrame panel=new JFrame("Capoo Game");
+        panel.setSize(1000,700);
+        panel.setVisible(true);
+        panel.setResizable(false);
+        panel.setLocationRelativeTo(null); 
+    }
+}
