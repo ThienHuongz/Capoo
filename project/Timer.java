250674342fb1caa5335ab5e9385a74d44ba1c7f3
meomun1 <maluong1504@gmail.com>
2023-03-05 23:28:13 +0700

    Update timer to the project

--- project/Timer.java
+++ project/Timer.java
@@ -2,4 +2,24 @@ package project;
 
 public class Timer{
 	
+	Timer timer;
+	int second;
+	
+	public void Timer()
+	{
+		
+	}
+	
+	// To count timer
+	public void simpleTimer()
+	{
+		timer = new Timer(1000, new ActionListener()) {
+			
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				second++;
+			}
+		});
+	}
 }
