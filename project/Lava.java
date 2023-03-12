51ff602567b559ed3799bec0bcb8f9abaf535a2a
Nhadtt <dinhthithanhnha31.07.03.vl@gmail.com>
2023-03-12 22:15:56 +0700

    add Fish and lava Object

--- project/Lava.java
+++ project/Lava.java
@@ -4,21 +4,23 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
-public class Lava implements object {
+public class Lava extends object{
     private BufferedImage lava[] = new BufferedImage[20];
 
     private static final int totalImageOfLava = 18;
-    private static final int animation = (int)(GamePanel.getFPS()/(int) (totalImageOfLava-3));
-
+    private static final int animation = (int)(GamePanel.getFPS()/totalImageOfLava)+10;
     private int counterStep=0,step = 0;
-    public Lava(){
+
+    public Lava(int x,int y){
+        super(x,y);
+        init();
+
 
     }
     public void init(){
         try {
-
             for (int i=0;i<totalImageOfLava;i++){
-                lava[i]=ImageIO.read(getClass().getResourceAsStream("../assets/lava/lava_sprite_0"+i+".png"));
+                lava[i]=ImageIO.read(getClass().getResourceAsStream("../assets/lava/lava_"+(i+1)+".png"));
             }
 
         } catch (Exception e) {
@@ -26,7 +28,9 @@ public class Lava implements object {
         }
     }
     public void draw(Graphics g){
-        g.drawImage(lava[step], 100, 200, null);
+        // g.drawImage(lava[step], 609, 522, null);
+        // g.drawImage(lava[step], 500, 134, null);
+        g.drawImage(lava[step], super.getX(), super.getY(), null);
 
     }
     public void update(){
@@ -34,12 +38,15 @@ public class Lava implements object {
         if (counterStep > animation){
             step++;
             counterStep=0;
-            if (step >= totalImageOfLava ){
-                step=0;
+            if (step >= totalImageOfLava-1 ){
+                step=1;
             }
             else step++;
         }
-        else counterStep++;
-        
+        else counterStep++;   
     }
+    public BufferedImage getImage(){
+        return lava[step];
+    }
+
 }
