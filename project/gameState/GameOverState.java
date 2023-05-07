ab4d60913dfab074669ed590daee096d05eb5278
meomun1 <maluong1504@gmail.com>
2023-05-07 23:02:57 +0700

    Change gameOver GUI

--- project/gameState/GameOverState.java
+++ project/gameState/GameOverState.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 
 
 public class GameOverState {
-    private BufferedImage mn[] = new BufferedImage[10];
+    private BufferedImage mn[] = new BufferedImage[20];
   
     GamePanel gamepanel;
 
@@ -31,15 +31,21 @@ public class GameOverState {
         try {
         	mn[0] = ImageIO.read(getClass().getResourceAsStream("/assets/start1.png"));
         	mn[1] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/game_over.png"));
-        	mn[2] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/decorate.png"));
+        	mn[2] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/deadState/cloud.png"));
         	mn[3] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/exit.png"));
         	mn[4] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/menu.png"));
         	mn[9] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/start.png"));
         	
-            mn[5] = ImageIO.read(getClass().getResourceAsStream("../../assets/name1.png"));
-            mn[6] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/Star1.png"));
-            mn[7] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/Star2.png"));
-            mn[8] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/Star3.png"));
+            mn[5] = ImageIO.read(getClass().getResourceAsStream("../../assets/gameOverName.png"));
+            mn[6] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/deadState/CapooDead1.png"));
+            mn[7] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/deadState/CapooDead2.png"));
+            mn[8] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/deadState/CapooDead3.png"));
+            mn[10] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/deadState/CapooDead4.png"));
+            mn[11] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/deadState/CapooDead5.png"));
+            mn[12] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/deadState/CapooDead6.png"));
+            mn[13] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/deadState/CapooWin1.png"));
+            mn[14] = ImageIO.read(getClass().getResourceAsStream("../../assets/endState/deadState/CapooWin2.png"));
+
 
         } catch (IOException e) {
             System.err.println("Error loading map from file: " + e.getMessage());
@@ -53,16 +59,20 @@ public class GameOverState {
         g.fillRect(0, 0, game.getScreenWidth(), game.getScreenHeight());
         
         g.drawImage(mn[5], (game.getScreenWidth() - mn[5].getWidth()) / 2, 50, null);
-        g.drawImage(mn[2],  0, game.getScreenHeight()/2, null);
-        // Exit
+        g.drawImage(mn[2], 0, game.getScreenHeight()/2-150, null);
+        g.drawImage(mn[2], game.getScreenWidth() - mn[2].getWidth(), game.getScreenHeight()/2-150, null);
+//        g.drawImage(mn[1], (game.getScreenWidth() - mn[1].getWidth()) / 2, game.getScreenHeight()-250, null);
+
         g.drawImage(mn[3], (game.getScreenWidth() - mn[3].getWidth()) / 2, 400, null);
-        // Menu
-//        g.drawImage(mn[4], (game.getScreenWidth() - mn[4].getWidth()) / 2, 400, null);
-        //Start
+
         g.drawImage(mn[4], (game.getScreenWidth() - mn[4].getWidth()) / 2, 300, null);
         
-        g.drawImage(mn[1], 100, 200, null);
-        g.drawImage(mn[1], 600, 200, null);
+        g.drawImage(mn[6], 50, 200, null);
+        g.drawImage(mn[7], 830, 200, null);
+        g.drawImage(mn[8], 230, 470, null);
+        g.drawImage(mn[10], 650, 450, null);
+        g.drawImage(mn[11], 150, 330, null);
+        g.drawImage(mn[12], 750,300, null);
         
         //Retry
         
