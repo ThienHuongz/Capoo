3b317c1828d25260486663f76b180c4d0996aa52
Thienhuongz <huongy138@gmail.com>
2023-05-14 22:37:38 +0700

    GameStateBase interface extends Base

--- project/gameState/GameStateBase.java
+++ project/gameState/GameStateBase.java
@@ -1,11 +1,6 @@
 package project.gameState;
-
-public interface GameStateBase {
-    void init();
-
-    void draw(java.awt.Graphics g);
-
-    void update();
+import project.Base;
+public interface GameStateBase extends Base{
 
     void mouse_click(int mx, int my);
     void mouse_move(int mx, int my);
